6b27ba3e285f95ab8cf1cdc2a03b486ee889d724
Magnus Groß <magnus.gross@rwth-aachen.de>
2020-12-03 13:11:51 +0100

    Use $ for readability

--- 2020/src/03/main.hs
+++ 2020/src/03/main.hs
@@ -4,10 +4,10 @@ main = do
     print(numtrees l 3 1 0)
 
     let list = [(1,1),(3,1),(5,1),(7,1),(1,2)]
-    print(product (map (\(r,d) -> numtrees l r d 0) list))
+    print $ product $ map (\(r,d) -> numtrees l r d 0) list
 
 numtrees :: [String] -> Int -> Int -> Int -> Int
 numtrees (a:as) r d x = let istree = (a!!x) == '#'
-                    in fromEnum istree + (numtrees (drop (d-1) as) r d ((x+r) `mod` length(a)))
+                    in fromEnum istree + numtrees (drop (d-1) as) r d ((x+r) `mod` length(a))
 numtrees [] _ _ _ = 0
 
