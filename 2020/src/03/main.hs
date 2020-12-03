9827e61382cfccfe155b47d3d4334b2144dac5bd
Magnus Groß <magnus.gross@rwth-aachen.de>
2020-12-03 12:36:24 +0100

    Next time someone asks you how to fix an infinite loop, remember this commit

---
+++ 2020/src/03/main.hs
@@ -0,0 +1,13 @@
+main = do
+    contents <- readFile "input.txt"
+    let l = lines contents
+    print(numtrees l 3 1 0)
+
+    let list = [(1,1),(3,1),(5,1),(7,1),(1,2)]
+    print(product (map (\(r,d) -> numtrees l r d 0) list))
+
+numtrees :: [String] -> Int -> Int -> Int -> Int
+numtrees (a:as) r d x = let istree = (a!!x) == '#'
+                    in fromEnum istree + (numtrees (drop (d-1) as) r d ((x+r) `mod` length(a)))
+numtrees [] _ _ _ = 0
+
