9ce6e59828d1a47554c99ebe001112eeb519bbbb
Magnus Groß <magnus.gross@rwth-aachen.de>
2020-12-03 13:18:06 +0100

    dito

--- 2020/src/03/main.hs
+++ 2020/src/03/main.hs
@@ -1,7 +1,7 @@
 main = do
     contents <- readFile "input.txt"
     let l = lines contents
-    print(numtrees l 3 1 0)
+    print $ numtrees l 3 1 0
 
     let list = [(1,1),(3,1),(5,1),(7,1),(1,2)]
     print $ product $ map (\(r,d) -> numtrees l r d 0) list
@@ -10,4 +10,3 @@ numtrees :: [String] -> Int -> Int -> Int -> Int
 numtrees (a:as) r d x = let istree = (a!!x) == '#'
                     in fromEnum istree + numtrees (drop (d-1) as) r d ((x+r) `mod` length(a))
 numtrees [] _ _ _ = 0
-
