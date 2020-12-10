416c7f94e2b992e9e7178d092252c5089f4140ff
Magnus Groß <magnus.gross@rwth-aachen.de>
2020-12-10 21:56:43 +0100

    Don't use EXPTIME

--- 2020/10/main.hs
+++ 2020/10/main.hs
@@ -10,16 +10,17 @@ diff x = zipWith (-) (drop 1 x) x
 count :: [Int] -> Int -> Int
 count x a = length $ filter (a==) x
 
-comb :: [Int] -> Int
-comb (x:y:z:xs) = let n = comb (y:z:xs)
-                    in case () of
-                      () | z - x <= 3 -> n + comb (x:z:xs)
-                      () | otherwise -> n
-comb _ = 1
+build :: [Int] -> [(Int, Int)]
+build (x:xs) = (x,1):[(a,0) | a <- xs]
+
+comb :: [(Int,Int)] -> [(Int,Int)]
+comb ((a,b):xs) = let n = [(c,d) | (c,d) <- xs, c - a <= 3]
+                  in ((a,b): comb([(c,d + b) | (c,d) <- n] ++ (xs \\ n)))
+comb _ = []
 
 main = do
     contents <- readFile "input.txt"
     let a = adapters $ lines contents
     let d = diff a
     print $ (count d 1) * (count d 3)
-    print $ comb a
+    print $ snd $ last $ comb $ build a
