885edf807218f6c1e51cb7ece43f864e88a0f690
Magnus Groß <magnus.gross@rwth-aachen.de>
2021-12-09 03:25:47 +0100

    Refactor

--- 2021/08/main.hs
+++ 2021/08/main.hs
@@ -9,6 +9,11 @@ import Data.Maybe
 -- 4    5
 --  6666
 
+-- digits that have unique segment count
+constant_rules  = [(1,2), (4,4), (7,3), (8,7)]
+-- direct inference rules for digits with ambiguous segment count
+inference_rules = [(5, ([(1,2,3),(4,3,5)],2)), (6, ([(1,1,6),(4,4,9)], 0))]
+
 -- (d,c,r): If there is a digit in ruleset rs, with exactly c common segments, then the correct number is r. If none matches, use f as fallback
 match :: String -> [(String, Int)] -> ([(Int, Int, Int)], Int) -> Int
 match x rs ((d,c,r):bs, f) = case (c == (length $ intersect (fst $ fromJust $ find ((== d) . snd) rs) x)) of
@@ -17,11 +22,8 @@ match x rs ((d,c,r):bs, f) = case (c == (length $ intersect (fst $ fromJust $ fi
 match _ _ ([], f) = f
 
 get :: [String] -> [(String, Int)] -> [(String, Int)]
-get xs [] = get (concat $ [filter ((== k) . length) xs | k <- [5,6]]) [(fromJust $ find ((== b) . length) xs, a) | (a,b) <- [(1,2), (4,4), (7,3), (8,7)]]
-get (x:xs) rs = let b = case (length x) of
-                          5 -> ([(1,2,3),(4,3,5)], 2)
-                          6 -> ([(1,1,6),(4,4,9)], 0)
-                        in get xs ((x, match x rs b):rs)
+get xs [] = get (concat $ [filter ((== k) . length) xs | k <- map fst inference_rules]) [(fromJust $ find ((== b) . length) xs, a) | (a,b) <- constant_rules]
+get (x:xs) rs = get xs ((x, match x rs $ snd $ fromJust $ find (\a -> fst a == length x) inference_rules):rs)
 get _ rs = rs
 
 value :: [String] -> [(String, Int)] -> Int
@@ -32,6 +34,6 @@ main = do
     contents <- readFile "input.txt"
     let l = lines contents
     -- part 1
-    print $ length $ filter ((`elem` [2,3,4,7]) . length) $ concat $ map (words . (drop 2) . dropWhile (/= '|')) l
+    print $ length $ filter ((`elem` map snd constant_rules) . length) $ concat $ map (words . (drop 2) . dropWhile (/= '|')) l
     -- part 2
     print $ sum $ map (\s -> value (reverse $ words $ drop 2 $ dropWhile (/= '|') s) $ get (words $ takeWhile (/= '|') s) []) l
