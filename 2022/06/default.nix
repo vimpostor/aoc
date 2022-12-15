e5e183499f820e16a9bc1c708a32bcab9087c897
Magnus Groß <magnus.gross@rwth-aachen.de>
2022-12-15 23:49:52 +0100

    Tuning trouble

---
+++ 2022/06/default.nix
@@ -0,0 +1,10 @@
+with (import <nixpkgs/lib>);
+with (import ../lib);
+let
+	parsed = init (stringToCharacters (readFile ./input.txt));
+	find = l: n: if length (unique (take n l)) == n then n else 1 + find (drop 1 l) n;
+	part1 = find parsed 4;
+	part2 = find parsed 14;
+in {
+	inherit part1 part2;
+}
