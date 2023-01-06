03d56dd19cdd635d5cf9f1d5ee217121af4cf6dc
Magnus Groß <magnus.gross@rwth-aachen.de>
2023-01-07 00:03:29 +0100

    Invert heights for part2
    Also use BFS.

--- 2022/12/default.nix
+++ 2022/12/default.nix
@@ -10,18 +10,26 @@ let
 	findchar = g: c: let a = map (y: findIndex c y) g; in { x = head (dropWhile (x: x >= infty) a); y = length (takeWhile (x: x >= infty) a); };
 	start = findchar g startc;
 	end = findchar g endc;
-	p = gridSetAt (gridSetAt g start.x start.y (strings.charToInt "a")) end.x end.y (strings.charToInt "z");
-	neighbors = d: x: y:
+	u = gridSetAt (gridSetAt g start.x start.y (strings.charToInt "a")) end.x end.y (strings.charToInt "z");
+	neighbors = d: x: y: p:
 		let
 			o = [{ x = x - 1; y = y; } { x = x + 1; y = y; } { x = x; y = y - 1; } { x = x; y = y + 1; }];
 		in
 			filter (c: abs (c.x - xl) <= xl && abs (c.y - yl) <= yl && gridAt d c.x c.y == infty && (gridAt p c.x c.y - gridAt p x y < 2)) o;
-	dijkstra = d: x: y: a: b: if x == a && y == b then 0 else
+	dijkstra = q: d: a: b: z: p:
 		let
-			r = map (c: 1 + dijkstra (gridSetAt d c.x c.y (1 + gridAt d x y)) c.x c.y a b) (neighbors d x y);
+			h = head q;
+			l = neighbors d h.x h.y p;
+			e = foldl' (l: n: gridSetAt l n.x n.y (1 + gridAt l h.x h.y)) d l;
+			s = (forward q) ++ map (c: { x = c.x; y = c.y; n = 1 + h.n; } ) l;
 		in
-			if empty r then infty else min r;
-	part1 = dijkstra d start.x start.y end.x end.y;
+			if (h.x == a && h.y == b) || (z && gridAt p h.x h.y == -(strings.charToInt "a")) then
+				h.n
+			else
+				dijkstra s e a b z p;
+	part1 = dijkstra [({ x = start.x; y = start.y; n = 0; })] d end.x end.y false u;
+	v = map (y: map (x: -x) y) u;
+	part2 = dijkstra [({ x = end.x; y = end.y; n = 0; })] d start.x start.y true v;
 in {
-	inherit part1;
+	inherit part1 part2;
 }
